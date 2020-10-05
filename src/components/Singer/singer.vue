e2fb992d3db8594bb58b61aa0bd8b70f78699b7a
wei <15810856771@139.com>
2020-10-05 10:58:30 +0800

    feat:get singers

--- src/components/Singer/singer.vue
+++ src/components/Singer/singer.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-      qqq222
+  <div class="singer">
+
   </div>
 </template>
+<script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res, '===')
+          this.singers = res.data.list
+        }
+      })
+    }
+  }
+
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
