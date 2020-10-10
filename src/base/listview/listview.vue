967b4118faafc83c93483b4161e0e0db449a5acd
wei <15810856771@139.com>
2020-10-10 11:30:00 +0800

    feat:singer title fixed

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -3,6 +3,7 @@
     :data="data"
     :listen-scroll="listenScroll"
     :probe-type="probeType"
+    @scroll="scroll"
     class="listview"
     ref="listview">
     <ul>
@@ -20,10 +21,18 @@
     </ul>
     <div class="list-shortcut" @touchmove.stop.prevent="onShortCutListTouchMove" @touchstart="onShortCutListTouchStart">
       <ul>
-        <li v-for="(item,index) in shortcutList" :key="index" class="item" :data-index="index">
+        <li
+          v-for="(item,index) in shortcutList"
+          :key="index"
+          class="item"
+          :class="{'current' : currentIndex == index }"
+          :data-index="index">
           {{item}}
         </li>
       </ul>
+    </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <div class="fixed-title">{{fixedTitle}}</div>
     </div>
   </scroll>
 </template>
@@ -32,6 +41,7 @@ import scroll from 'base/scroll/scroll'
 import {getData} from 'common/js/dom'
 
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 
 export default {
   props: {
@@ -45,6 +55,12 @@ export default {
       return this.data.map((item) => {
         return item.title.substring(0, 1)
       })
+    },
+    fixedTitle () {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   components: {
@@ -52,7 +68,10 @@ export default {
   },
   data () {
     return {
-
+      scrollY: -1,
+      currentIndex: 0,
+      listHeight: [],
+      diff: 0
     }
   },
   created () {
@@ -60,10 +79,40 @@ export default {
     this.listenScroll = true
     this.touch = {}
   },
-  methods: {
-    listenScroll () {
-
+  watch: {
+    data () {
+      setTimeout(() => {
+        this._calculateHeight()
+      }, 20)
     },
+    scrollY (newY) {
+      let listHeight = this.listHeight
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      }
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i]
+        let height2 = listHeight[i + 1]
+        if (-newY >= height1 && -newY < height2) {
+          this.currentIndex = i
+          this.diff = height2 + newY
+          return
+        }
+      }
+
+      this.currentIndex = listHeight.length - 2
+    },
+    diff (newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+    }
+  },
+  methods: {
     onShortCutListTouchStart (e) {
       let anchorIndex = getData(e.target, 'index')
       this.touch.y1 = e.touches[0].pageY
@@ -75,6 +124,20 @@ export default {
       let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
       let anchorIndex = parseInt(delta) + parseInt(this.touch.anchorIndex)
       this._scrollTo(anchorIndex)
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    _calculateHeight () {
+      let list = this.$refs.listGroup
+      this.listHeight = []
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < list.length; i++) {
+        let val = list[i]
+        height += val.clientHeight
+        this.listHeight.push(height)
+      }
     },
     _scrollTo (index) {
       this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
