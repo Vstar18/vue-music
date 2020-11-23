a5f53a4e15a143bd55948261983ea4bc6a2e4363
wei <15810856771@139.com>
2020-11-23 19:15:53 +0800

    feat:add components music-list song-list

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer-detail">
-
+    <music-list :songs="songs" :title="title" :bgImage="bgImage"></music-list>
   </div>
 </template>
 
@@ -8,11 +8,28 @@
 import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/songs'
+import MusicList from 'components/music-list/music-list'
+
 export default {
+  components: {
+    MusicList
+  },
   computed: {
+    title: function () {
+      return this.singer.name
+    },
+    bgImage: function () {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
+  },
+  data () {
+    return {
+      songs: []
+    }
   },
   created () {
     this._getSingerDetail()
@@ -26,15 +43,34 @@ export default {
       }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
-
+          this.songs = this._normalizeSongs(res.data.list)
         }
-        console.log(res, '----')
+        console.log(this.songs, 'this.songs----')
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
 </script>
 
-<style>
-
-</style>template>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+.singer-detail {
+  position :fixed
+  z-index: 100
+  top:0
+  left: 0
+  right: 0
+  bottom: 0
+  background-color: $color-background
+}
+</style>
